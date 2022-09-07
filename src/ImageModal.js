d7bb2b3327632227a70371082af5812004d4454e
Quadatz <quadath@gmail.com>
2022-09-07 20:47:05 +0300

    modal image

--- src/ImageModal.js
+++ src/ImageModal.js
@@ -15,12 +15,12 @@ export default function ImageModal({ isVisible, id, closeModal }) {
     }, [isVisible, id])
 
     return (
-        <div className="modal-wrapper">
-            <div className={isVisible ? "image-modal__bg" : "hidden"} onClick={closeModal}/>
+        <div className={isVisible ? "image-modal__bg" : "hidden"} onClick={closeModal}>
             <div className={isVisible ? "image-modal active" : "image-modal"}>
                 {loading ? <p>Loading...</p> : null}
-                <img classname="image-modal__img" src={data.url} alt="big"/>
+                <img className="image-modal__img" src={data.url} alt="big" />
             </div>
+        <div />
         </div>
 
     )
